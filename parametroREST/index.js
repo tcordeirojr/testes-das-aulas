e92e51b05126ff17649c0b1f5318dc462baf8676
tcordeirojr <tedcordeirojr@gmail.com>
2024-03-07 02:08:22 -0300

    parametro rest

---
+++ parametroREST/index.js
@@ -0,0 +1,9 @@
+//function(a, b, ...args) {
+    //
+//} 
+
+function incentivarQuester(mensagem, ...nomesQuesters){
+    nomeQuesters.map(nomeQuester => console.log (`${mensagem} ${nomeQuester}`))
+}
+
+incentivarQuester("Parabéns por ter chego ao módulo de JavaScript avançado,", 'João','Pedro', 'rafael', 1,true)
