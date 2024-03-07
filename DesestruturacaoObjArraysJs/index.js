1f3b244aaa17cbd0a9cdedf3058acd316569e9c4
tcordeirojr <tedcordeirojr@gmail.com>
2024-03-07 01:17:56 -0300

    desestruturação de objetos e arrays

---
+++ DesestruturacaoObjArraysJs/index.js
@@ -0,0 +1,24 @@
+let pessoa = { nome: 'Ted', sobrenome: 'Cordeiro', idade: 26 }
+
+// let nome = pessoa.nome
+// let idade = pessoa.idade
+
+// console.log(nome)
+// console.log(idade)
+
+// let{ nome, idade, sobrenome} = pessoa 
+// console.log(nome)
+// console.log(idade)
+// console.log(sobrenomeDaPessoa)
+
+// const numeros = [1, 2, 3];
+
+// const [ um, dois, tres, quatro ] = numeros
+
+// console.log(um)
+// console.log(dois)
+// console.log(tres)
+// console.log(quatro)
+
+
+//let{ nome: nome, idade: idade, sobrenomeDaPessoa:sobrenome}
