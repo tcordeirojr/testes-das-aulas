596f8a7914cfd359c26083affd0e38f69d8556e1
tcordeirojr <tedcordeirojr@gmail.com>
2024-03-07 01:39:21 -0300

    operador spread

---
+++ operador spread/index.js
@@ -0,0 +1,21 @@
+const pessoas = [ 'Ted', 'Giulia', 'Helena']
+console.log(...pessoas)
+
+// CONCATENAR DOIS ARRAYS 
+
+let pessoas1 = [ 'Ted', 'Giulia', 'Helena']
+let pessoas2 = [ 'Matheus', 'Sandra', 'TedPai']
+
+pessoas1 = pessoas1.concat(pessoas2)
+
+pessoas1 = [...pessoas1, ...pessoas2, ...pessoas3]
+console.log(pessoas1)
+
+//CLONAR OBJETOS
+
+const pessoa1 = {nome:'Ted', idade:26}
+const pessoa2 = {nome:'Giulia' idade:28}
+
+const objetoClonado = { ...pessoa1}
+console.log(objetoClonado)
+console.log(pessoa1)
